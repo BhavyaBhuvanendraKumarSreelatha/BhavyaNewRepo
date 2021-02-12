a9c8fa5e2798826f5181fcca6ebb9049e929f655
Bhavya Bhuvanendra Kumar Sreelatha <bsreelat@my.centennialcollege.ca>
2021-02-13 01:03:31 +0530

    initial commit

---
+++ public/Scripts/app.js
@@ -0,0 +1,15 @@
+/*  File Name: app.js
+    Student's Name: Bhavya Bhuvanendra Kumar Sreelatha
+    Student ID: 301118501
+    Date: 13/02/2021
+*/
+(function(){
+    function Start(){
+
+console.log("App Started...")
+
+    }
+
+    window.addEventListener("load", Start);
+    
+})();
